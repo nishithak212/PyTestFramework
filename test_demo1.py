c7253ce0129fefcf5513a253b4724914c520b480
nishithak212 <knishitha95@gmail.com>
2026-03-10 15:52:25 -0400

    Commads to run pytest from command line

--- test_demo1.py
+++ test_demo1.py
@@ -7,4 +7,10 @@
 def test_firstProgram(): #Test method. Test method names should always start with test keyword
     print("Hello Hi")
 
+#Commands to run pytest from terminal
+
+#To run large number of test cases:
+#1. pytest == Goes to the directory to find all pytest files
+#2. pytest -v == v stands for verbose. Gives more information about test case results execution
+#3. pytest -v --s == s is used to print all console logs
 
