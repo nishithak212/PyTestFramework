1356567003c02febeccf87b7f12016c0dce6f8f7
nishithak212 <knishitha95@gmail.com>
2026-03-10 15:47:45 -0400

    First PyTest demo

---
+++ test_demo1.py
@@ -0,0 +1,10 @@
+#Any pytest file should start with test_ or end with _test keyword
+
+#As per pytest framework standards, code should be written in functions only
+
+#Syntax of funtion in python. Everyline of code should be written in function only if using Pytest framework
+
+def test_firstProgram(): #Test method. Test method names should always start with test keyword
+    print("Hello Hi")
+
+
